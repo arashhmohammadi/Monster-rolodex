a000e50cef570d9e8ef84a3ede6808cbb6bb5600
Arash Mohammadi <arash.m1379@yahoo.com>
2020-05-29 12:18:55 +0200

    Updates

---
+++ precache-manifest.56cbf0820c8dfc45ad7201d9787a6e17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d613d385de7a88e3eaa5fea85e890d4",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4af9b4756b05087ea229",
+    "url": "/monster-rolodex/static/css/main.537777e6.chunk.css"
+  },
+  {
+    "revision": "0105f5fa7a131123a102",
+    "url": "/monster-rolodex/static/js/2.8a77c072.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.8a77c072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4af9b4756b05087ea229",
+    "url": "/monster-rolodex/static/js/main.ef4e6ecf.chunk.js"
+  },
+  {
+    "revision": "753b30f80137f55e537c",
+    "url": "/monster-rolodex/static/js/runtime-main.e7a3ea5b.js"
+  }
+]);
